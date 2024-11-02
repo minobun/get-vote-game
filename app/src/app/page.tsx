52b1729d1fed7e57527dbef819ebe631e3a6e13a
Naoya Minomura <naotoitoi@gmail.com>
2024-11-02 09:39:30 +0900

    地図生成初期

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,95 +1,72 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [map, setMap] = useState<number[][]>([[0]])
+  const createNumber = (x: number, y: number | undefined): number => {
+    const c = Math.random();
+    if (y === undefined) {
+      if (x === 0) {
+        if (c < 0.5) return x;
+        else return x + 1;
+      }
+      else if (x === 2) {
+        if (c < 0.5) return x - 1;
+        else return x;
+      }
+      else {
+        if (c < 0.5) return x;
+        else if (c < 0.75) return x + 1;
+        else return x - 1;
+      }
+    }
+    else {
+      if (x === y) {
+        if (c < 0.8) return x;
+        if (x === 2) return x - 1;
+        if (c < 0.9) return x + 1;
+        if (x === 0) return x;
+        return x - 1;
+      }
+      if (x + 1 === y || x - 1 === y) {
+        if (c < 0.5) return y;
+        return x;
+      }
+      if (x + 2 === y || x - 2 === y) {
+        return (x + y) / 2;
+      }
+    }
+    return x;
+  }
+  const createMap = () => {
+    const latestRow = map[map.length - 1];
+    const row = new Array(latestRow.length + 1).fill(0);
+    const newRow = row.map((cur, index) => {
+      if (index === 0) {
+        return createNumber(latestRow[0], undefined);
+      }
+      if (index === row.length - 1) {
+        return createNumber(latestRow[index - 1], undefined);
+      }
+      return createNumber(latestRow[index], latestRow[index + 1])
+    })
+
+    setMap((cur) => [...cur, newRow]);
+  }
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <button onClick={createMap}>生成</button>
+      {
+        map.map((cur, index) => (
+          <div key={`${index}列目`}>
+            {
+              cur.map((num, i) => (<span key={`${i}番目`}>{num}</span>))
+            }
+          </div>
+        ))
+      }
     </div>
   );
 }
